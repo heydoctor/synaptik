907f880482781a1dbc5241981aed0dd3b108b7ad
Kyle Alwyn <kjalwyn@gmail.com>
2018-04-27 23:31:08 -0700

    Remove jest setup files

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,11 @@ module.exports = {
   "collectCoverageFrom": [
     "src/**/*.{js,jsx,mjs}"
   ],
-  "setupFiles": [
-    "<rootDir>/config/polyfills.js"
-  ],
   "testMatch": [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}",
     "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}"
   ],
   "testEnvironment": "node",
-  "testURL": "http://localhost",
-  "transform": {
-    "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/babel-jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
-  },
   "transformIgnorePatterns": [
     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"
   ],
