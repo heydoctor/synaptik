fa040977bb0760df9e87e7466517998798d1f224
Kyle Alwyn <kjalwyn@gmail.com>
2018-04-29 22:48:10 -0700

    fix(logger): use cjs

--- logger.js
+++ logger.js
@@ -1,4 +1,4 @@
-import { detailedDiff } from 'deep-object-diff';
+const detailedDiff = require('deep-object-diff').detailedDiff;
 
 const logger = (message, color, object) => {
   if (object && Object.keys(object).length) {
@@ -18,7 +18,7 @@ export default function logStateChange(prevState, newState) {
   logger('Added', '#18A76D', diff.added);
   logger('Updated', '#FDA429', diff.updated);
   logger('Deleted', '#f04545', diff.deleted);
-  logger('Old state: ', '#555555', { ...prevState });
-  logger('New state: ', '#555555', { ...newState });
+  logger('Old state: ', '#555555', Object.assign({}, prevState));
+  logger('New state: ', '#555555', Object.assign({}, newState))
   console.groupEnd(groupName);
 };
