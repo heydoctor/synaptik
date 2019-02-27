2cb22a64d4d1acbd38c5a0e04d42eb8505110715
Kyle Alwyn <kjalwyn@gmail.com>
2019-02-27 15:43:11 -0800

    Remove connect component and HOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import Connect, { connect } from './Connect';
+import Synapse from './Synapse';
 import Provider from './Provider';
 import Store from './Store';
 import useSynapse from './useSynapse';
-import Synapse from './Synapse';
 
-export { Synapse, Provider, Connect, connect, Store, useSynapse };
+export { Synapse, Provider, Store, useSynapse };
